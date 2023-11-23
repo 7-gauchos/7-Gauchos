65a9b1f377f240fc8e795dad6dddb83219debc1c
AleGM2010 <Alesandrogm@live.com>
2023-11-23 14:24:36 -0300

    Limpiar panel extra de stats de personaje (#23)
    -Eliminar en Panel los Stats de felicidad.
    -Modificar en Drop Dinero para que muestre 0 o cant a ganar x jugador.

--- Assets/Scripts/Vista/Drop.cs
+++ Assets/Scripts/Vista/Drop.cs
@@ -8,7 +8,7 @@ public class Drop : MonoBehaviour, IDropHandler {
     public bool espacio_Ocupado_ = false; // Bool para registrar si el objeto ha sido colocado en un panel.
 
     [SerializeField] TextMeshProUGUI texto_dinero;
-    [SerializeField] TextMeshProUGUI texto_felicidad;
+
 
     public void OnDrop(PointerEventData eventData) {
         var cartaPresionada = eventData.pointerDrag.transform.GetComponent<Carta_Accion>();
@@ -35,17 +35,15 @@ public class Drop : MonoBehaviour, IDropHandler {
     // Si no es nulo, actualize esos valores y tambien si no es nulo La carta (osea tiene hija)
 
     private void Update() {
-        if (texto_dinero != null && texto_felicidad != null) {
+        if (texto_dinero != null) {
             Personaje scripPersonaje = GetComponent<Personaje>();
             // Si este objeto, tiene una hija y por lo tanto esta ocupado
             if (espacio_Ocupado_ && transform.childCount == 1) {
                 // Mostrar cambios de el Personaje y la Accion conjuntas.
 
                 texto_dinero.text = (scripPersonaje.multiplicadorDinero * transform.GetChild(0).GetComponent<Accion>().Dinero).ToString();
-                texto_felicidad.text = (scripPersonaje.felicidad + transform.GetChild(0).GetComponent<Accion>().Costo_felicidad).ToString();
             } else {
-                texto_dinero.text = scripPersonaje.multiplicadorDinero.ToString();
-                texto_felicidad.text = scripPersonaje.felicidad.ToString();
+                texto_dinero.text = "0";
             }
             // IMPORTANTE : El texto del dinero por ganar al colocar la carta
             //              es el mismo que el multiplicador sin tener carta encima
