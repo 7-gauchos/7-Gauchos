af59e65d4f6cb0c5165314c43c5387e843f0a2fb
Ezequiel Bertone <ezikoh@gmail.com>
2023-12-01 10:10:43 -0300

    860rzp0kw : Implementar seleccion de mision individual (#32)
    * 860rzp0kw : Implementar seleccion de mision individual
    
    Se agrego seleccion de la mision
    Aparece el boton siguiente cuando se selecciona la mision
    Se agrego un marco de color cuando se selecciona la mison
    Se agrego el sonido del click al presionar los botones
    
    * 860rzp0kw : Implementar seleccion de mision individual
    
    Quedo un boton mal activado por eso este commit

--- Assets/Scripts/Vista/IraEscena.cs
+++ Assets/Scripts/Vista/IraEscena.cs
@@ -1,23 +1,46 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.SearchService;
 using UnityEngine;
-using UnityEngine.AI;
 using UnityEngine.SceneManagement;
 
 public class IraEscena : MonoBehaviour
 {
     public int nroescena;
+    public AudioSource sndButton;
 
     public void goToEscene()
     {
-        SceneManager.LoadScene(nroescena);
+        // si esta el sonido ejectura el sonido y llama a la escena, sino solo carga la escena
+        if (sndButton != null) {
+            StartCoroutine(ReproducirSonido(nroescena));
+        }
+        else
+        {
+            SceneManager.LoadScene(nroescena);
+        }
+
+        
     }
 
     public void goToPreviousEscene()
     {
-        SceneManager.LoadScene(nroescena-2);
+        if (sndButton != null) { 
+            StartCoroutine(ReproducirSonido(nroescena-2)); 
+        }
+        else { 
+            SceneManager.LoadScene(nroescena-2);
+        }
     }
 
+
+    private System.Collections.IEnumerator ReproducirSonido(int nroEscena)
+    {
+        // Reproduce el sonido
+        sndButton.Play();
+
+        // Espera hasta que el sonido haya terminado de reproducirse
+        yield return new WaitForSeconds(sndButton.clip.length);
+
+        // Carga la siguiente escena
+        SceneManager.LoadScene(nroEscena);
+    }
 
 }
