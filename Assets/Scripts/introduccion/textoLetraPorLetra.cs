0c782502218ed66249cedbd92887f7451dde3d95
Denis Peppino <49687480+dpeppino-incomm@users.noreply.github.com>
2023-12-10 16:44:59 -0300

    CU-860t2ktyk - Agregar un efecto al texto de la introduccion (#35)
    se agrego el codigo para difuminar cada parrafo por separado

--- Assets/Scripts/introduccion/textoLetraPorLetra.cs
+++ Assets/Scripts/introduccion/textoLetraPorLetra.cs
@@ -7,6 +7,7 @@ public class NewBehaviourScript : MonoBehaviour
 {
     public TextMeshProUGUI fraseCompleta;
     public TextMeshProUGUI textoEscrito;
+    public float tiempoDeDifuminacion = 2f;
     public float waitTime = 0.06f;
     public Button btnContinuar;
     public AudioSource audioTeclas;
@@ -15,18 +16,33 @@ public class NewBehaviourScript : MonoBehaviour
     void Start()
     {
         StartCoroutine(Reloj());
-        
     }
 
     IEnumerator Reloj()
     {
+        float desv = 0.12f;
+        float variacionDeTransparencia = 1/(tiempoDeDifuminacion / desv);
         audioTeclas.Play();
         foreach (char caracter in fraseCompleta.text.ToString())
         {
-            textoEscrito.text = textoEscrito.text + caracter;
-            yield return new WaitForSeconds(waitTime);
+            if (caracter == '\n') // difumino las letras
+            {
+                for (float i = 0; i < tiempoDeDifuminacion; i += desv)
+                {
+                    Color c = textoEscrito.color;
+                    c.a = c.a - variacionDeTransparencia;
+                    textoEscrito.color = c;
+                    yield return new WaitForSeconds(desv);
+                }
+                textoEscrito.text = "";
+                textoEscrito.color = Color.white;
+            }
+            else
+            {
+                textoEscrito.text = textoEscrito.text + caracter;
+                yield return new WaitForSeconds(waitTime);
+            }
         }
-
         // cuando termina de pasar las letras activa el boton para terminar
         audioTeclas.Stop();
         btnContinuar.gameObject.SetActive(true);
