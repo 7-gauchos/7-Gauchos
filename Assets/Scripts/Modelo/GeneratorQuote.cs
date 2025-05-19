d716296df37d9f282773ae2bd83d0bb21444271d
nferran-incomm <82890429+nferran-incomm@users.noreply.github.com>
2025-05-19 09:19:25 -0300

    860t22tjn: Arreglar errores de ortografia a las descripciones de accion (#37)

--- Assets/Scripts/Modelo/GeneratorQuote.cs
+++ Assets/Scripts/Modelo/GeneratorQuote.cs
@@ -6,126 +6,126 @@ public class GeneratorQuote
 {
     private List<string> F_Trabajo = new List<string>
    {
-    "Se adelantó la fecha de entrega",
-    "Perdí el trabajo del mes (ese pdf estaba raro)",
-    "Una reunión de 15min duró todo el día",
-    "Toqué el enchufe que estaba en corto",
-    "No pude almorzar",
-    "Perdí mi taza de café",
-    "Jornada normal, 8 horas y a al rancho",
-    "La reunión de 15min, duró 15min",
-    "Por mantenimiento, la jornada se redujo media hora",
-    "Almorcé gratis con la tarjeta de la empresa",
-    "Mi jefe me regaló una taza",
-    "Mi jefe me dio la razón",
-    "Atendí un solo cliente",
-    "Declararon asueto de medio día"
+    "la fecha de entrega se adelantó",
+    "perdí todo el trabajo del mes",
+    "una reunión de 15 minutos duró todo el día",
+    "toqué el enchufe que estaba en corto",
+    "no pude almorzar",
+    "perdí mi taza de café favorita",
+    "tuve una jornada de trabajo normal. 8 horas y pal rancho",
+    "la reunión de 15 minutos duró 15 minutos",
+    "por mantenimiento la jornada laboral se redujo media hora",
+    "almorcé gratis con la tarjeta de la empresa",
+    "mi jefe me regaló una taza",
+    "mi jefe me dio la razón",
+    "atendí un solo cliente en todo el dia",
+    "declararon asueto de medio día"
 };
 
     private List<string> F_Ocio = new List<string>
     {
-    "Se suspendió el recital por lluvia",
-    "Manché mi remera de la suerte",
-    "Se olvidaron de mí jugando a la escondida, salí a las 2 horas",
-    "Mi equipo favorito perdió la final",
-    "Se cortó la luz",
-    "Me cancelaron a último momento",
-    "Navegué en el internet",
-    "Me puse a ver tele",
-    "Salí a caminar",
-    "Tomé mate con un amigo",
-    "Fui al cine, me encantó la película",
-    "Ganó Boca",
-    "Tarde de juegos de mesa",
-    "Gané el torneo"
+    "se suspendió el recital por lluvia",
+    "manché mi remera de la suerte",
+    "se olvidaron de mí jugando a la escondida. salí a las 2 horas",
+    "mi equipo favorito perdió la final",
+    "se cortó la luz mientras jugaba al lig of Leyend",
+    "me cancelaron la cita a último momento",
+    "navegué en internet mirando ",
+    "me puse a ver tele",
+    "salí a caminar y tomar aire",
+    "tomé mate con un amigo",
+    "fui al cine. Me encantó la película",
+    "ganó Boca",
+    "tuve una genial tarde de juegos de mesa",
+    "gané el torneo de microfutbol"
 };
 
 
     private List<string> F_Descanso = new List<string>
  {
-    "Dormí torcido",
-    "Tuve una pesadilla",
-    "Ganó Pachorra",
-    "Dormí siesta",
-    "Me quedé colgado mirando por la ventana",
-    "Dormí 8 horas"
+    "dormí torcido. Tengo contracturas por doquier",
+    "tuve una pesadilla",
+    "me gano la pachorra y me tuve que tirar a no hacer nada",
+    "dormí una muy buena siesta",
+    "me quedé mirando por la ventana",
+    "dormí 8 horas"
 };
 
     private List<string> F_Suerte = new List<string>
     {
-    "Gané una apuesta en la carrera de caracoles",
-    "La factura de internet estaba mal, no tuve que pagar",
-    "La abuela me mandó morfi",
-    "Soñé con Messi"
+    "gané una apuesta en la carrera de caracoles",
+    "la factura de internet estaba mal asi que no tuve que pagar",
+    "la abuela me mandó morfi",
+    "soñé con Messi"
 };
 
     private List<string> F_Desastre = new List<string>
     {
-    "Se desbordó el río, se inundó la casa",
-    "Terremoto, casa nueva",
-    "Perdí las llaves del auto",
-    "Me mordió un perro"
+    "se desbordó el río inundando toda la casa",
+    "Sobreviví a un terremoto. La casa no sobrevivió",
+    "perdí las llaves del auto",
+    "me mordió un perro.  "
 };
 
     private List<string> D_Trabajo = new List<string>
     {
-    "Quemé la computadora del trabajo",
-    "Desenchufé la heladera, la comida se echó a perder",
-    "Tuve un accidente laboral (no me lo cubren)",
-    "Presenté los viáticos fuera de fecha",
-    "Llegué tarde, me descontaron",
-    "Rompió algo en el trabajo",
-    "Me rajaron",
-    "Periodo de prueba en el laburo nuevo",
-    "Una changüita",
-    "Vendí algo de la casa",
-    "\"Trabajo, muy duro, como un esclavo\"",
-    "Cumplí el objetivo (palmaditas en la espalda)",
-    "Cobré el Aguinaldo",
-    "Hice horas extra, las pizzas estaban muy buenas"
+    "quemé la computadora del trabajo",
+    "olvidé la heladera desenchufada. La comida se echó a perder",
+    "tuve un accidente laboral. La empresa se lavó las manos",
+    "presenté los viáticos fuera de fecha",
+    "llegué tarde otra vez. Me descontaron del sueldo.",
+    "rompi la cafetera del trabajo",
+    "me rajaron del laburo",
+    "arranqué con el periodo de prueba en un laburo nuevo",
+    "me salió una changuita",
+    "vendí algo de la casa",
+    "trabaje super duro, como esclavo. Gané platita!",
+    "cumplí el objetivo semanal en el trabajo (\"palmaditas en la espalda\")",
+    "cobré el Aguinaldo",
+    "hice horas extra.  Las pizzas que nos dieron estaban muy buenas"
 };
 
     private List<string> D_Ocio = new List<string>
     {
-    "El casino hizo de las suyas",
-    "Compré un juego, descubrí un nuevo impuesto",
-    "Pelotazo en la cara: lentes nuevos",
+    "el casino hizo de las suyas",
+    "compré un juego en Estim. Descubrí un nuevo impuesto",
+    "recibi un pelotazo en la cara por una atajada épica. Necesito lentes nuevos",
     "La fuente de la compu dijo basta",
-    "El cupón de descuento no descontó",
-    "Se venció la yerba: al almacén",
-    "Me invitaron a comer",
-    "Pasé por un festival en la plaza",
-    "Acerté todos los resultados del fútbol infantil",
-    "La subasta del cromo de Steam fue reñida",
-    "En el campeonato de truco gané el premio a la barba más larga",
-    "Mis bailes de TikTok están generando ingresos",
-    "Vendí una pintura del primario a una galería",
-    "Gané en el bingo"
+    "el cupón de descuento no descontó",
+    "se venció la yerba. Al almacén de nuevo",
+    "me invitaron a comer",
+    "pasé por un festival en la plaza",
+    "acerté todos los resultados de la liga de fútbol infantil",
+    "la subasta del cromo de Estim fue reñida",
+    "en el campeonato de truco gané el premio a la barba más larga",
+    "mis bailes de Tikitok están generando ingresos",
+    "vendí una pintura del primario a una galería",
+    "gané en el bingo"
 };
 
     private List<string> D_Descanso = new List<string>
     {
-    "Me descubrieron jugando en el trabajo.",
-    "Bajó el dólar: chau ahorros (1U$)",
-    "Acá tampoco pasó",
-    "No pasó nada",
-    "Bitcoin subió, ahora mis crypto valen 0,0000005% más",
-    "La abuela me dio plata como si fuera droga"
+    "me descubrieron jugando en el trabajo",
+    "bajó el dólar. Chau ahorros",
+    "me desperté de una siesta sin saber si era Martes, Junio o Invierno",
+    "no pasó nada",
+    "Shitcoin subió. Ahora mis crypto valen 0,0000005% más",
+    "la abuela me dio plata como si fuera droga"
 };
 
     private List<string> D_Suerte = new List<string>
     {
-    "Encontré una billetera (le dejé los documentos)",
-    "Gané la rifa asociación de canaricultores: una bolsa de alpiste",
+    "encontré una billetera (le dejé los documentos).",
+    "gané la rifa asociación de canaricultores: una bolsa de alpiste",
     "Le pifiaron el CBU: muchas gracias"
 };
 
     private List<string> D_Desastre = new List<string>
     {
-    "Me desbalijaron la casa",
-    "Las cubiertas originales, eran originales de China",
-    "AFIP: \"detectamo movimientos sospechosos\"",
-    "Chapuzón: teléfono incluido"
+    "me desvalijaron la casa.",
+    "las cubiertas originales eran originales pero de China.",
+    "Recibi un mail de la AFIP: \"detectamos movimientos sospechosos\"",
+    "me tire a la pileta con ropa. El telefono venia conmigo."
 };
 
 
@@ -164,14 +164,14 @@ public class GeneratorQuote
         if ((dinero < 0 && felicidad < 0) || (dinero >= 0 && felicidad >= 0))
         {
             // tomar ambas y sumarlas con "y"
-            auxString = auxT.Felicidad[auxIndiceFelicidad] + " y " + auxT.Dinero[auxIndiceDinero];
+            auxString = auxT.Felicidad[auxIndiceFelicidad] + ". Ademas " + auxT.Dinero[auxIndiceDinero];
         }
         else if ((dinero < 0 && felicidad >= 0) || (dinero >= 0 && felicidad < 0))
         {
             // tomar ambas y sumarlas con "pero"
-            auxString = auxT.Felicidad[auxIndiceFelicidad] + " pero " + auxT.Dinero[auxIndiceDinero];
+            auxString = auxT.Felicidad[auxIndiceFelicidad] + ". Pero  " + auxT.Dinero[auxIndiceDinero];
         }
-        return auxString;
+        return string.Concat(auxString[0].ToString().ToUpper(), auxString.Substring(1));
     }
 
     private int Mapeo(int v,int minV, int maxV,int minI,int maxI)
@@ -181,7 +181,7 @@ public class GeneratorQuote
         // Añadido valor absoluto por error de indice negativo , posible parche?
         auxI = ((v - minV) * (maxI - minI) / (maxV - minV) + minI);
         auxI = Math.Clamp(auxI,minI,maxI);
-        // Debug.Log("Indice nuevo: "+auxI);
+         Debug.Log("Indice nuevo: "+auxI);
         return auxI;
     }
 }
